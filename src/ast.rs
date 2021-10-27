500b51ffc5c9f34a2ca26acd6ca071dd87d2611e
tsatow <ta-satoh@atware.co.jp>
2021-10-28 00:36:05 +0900

    Boxを極力排除した

--- src/ast.rs
+++ src/ast.rs
@@ -1,13 +1,13 @@
 use std::collections::HashMap;
 
-pub enum TopLevel {
+pub enum TopLevel<'a> {
     GlobalVariableDefinition {},
-    FunctionDefinition {},
+    FunctionDefinition { name: &'a str, args: &'a [Expression<'a>], body: &'a Expression<'a> },
 }
 
 pub struct Environment<'a> {
-    bindings: &'a mut HashMap<&'a str, Value>,
-    next: Option<Expression<'a>>
+    pub bindings: &'a mut HashMap<&'a str, Value>,
+    pub next: Option<Expression<'a>>
 }
 
 pub struct Program {
@@ -15,7 +15,7 @@ pub struct Program {
 }
 
 impl Program {
-    pub fn definitions() -> &[TopLevel] {
+    pub fn definitions<'a>(&self) -> &[TopLevel<'a>] {
         todo!()
     }
 }
@@ -34,60 +34,60 @@ pub enum Operator {
 }
 
 pub enum Expression<'a> {
-    BinaryExpression { operator: Operator, lhs: Box<Expression<'a>>, rhs: Box<Expression<'a>> },
-    Assignment { name: &'a str, expr: Box<Expression<'a>> },
+    BinaryExpression { operator: Operator, lhs: &'a Expression<'a>, rhs: &'a Expression<'a> },
+    Assignment { name: &'a str, expr: &'a Expression<'a> },
     Identifier(&'a str),
     ValueExpression(Value),
     BlockExpression { expressions: &'a [Expression<'a>] },
-    WhileExpression { condition: Box<Expression<'a>>, body: Box<Expression<'a>> },
-    IfExpression { condition: Box<Expression<'a>>, then_clause: Box<Expression<'a>>, else_clause: Option<Expression<'a>> },
-    FunctionCall { name: &'a str, args: &'a [Expression<'a>], body: Box<Expression<'a>> },
+    WhileExpression { condition: &'a Expression<'a>, body: &'a Expression<'a> },
+    IfExpression { condition: &'a Expression<'a>, then_clause: &'a Expression<'a>, else_clause: &'a Option<Expression<'a>> },
+    FunctionCall { name: &'a str, args: &'a [Expression<'a>] },
 }
 
-impl Expression<'_> {
-    pub fn add<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Self <'a> {
-        Expression::BinaryExpression { operator: Operator::Add, lhs: Box::new(lhs), rhs: Box::new(rhs) }
+impl<'a> Expression<'a> {
+    pub fn add(lhs: &'a Self, rhs: &'a Self) -> Self {
+        Expression::BinaryExpression { operator: Operator::Add, lhs, rhs }
     }
-    pub fn subtract<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
-        Expression::BinaryExpression { operator: Operator::Subtract, lhs: Box::new(lhs), rhs: Box::new(rhs) }
+    pub fn subtract(lhs: &'a Self, rhs: &'a Self) -> Self {
+        Expression::BinaryExpression { operator: Operator::Subtract, lhs, rhs }
     }
-    pub fn multiply<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
-        Expression::BinaryExpression { operator: Operator::Multiply, lhs: Box::new(lhs), rhs: Box::new(rhs) }
+    pub fn multiply(lhs: &'a Self, rhs: &'a Self) -> Self {
+        Expression::BinaryExpression { operator: Operator::Multiply, lhs, rhs }
     }
-    pub fn divide<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
-        Expression::BinaryExpression { operator: Operator::Divide, lhs: Box::new(lhs), rhs: Box::new(rhs) }
+    pub fn divide(lhs: &'a Self, rhs: &'a Self) -> Self {
+        Expression::BinaryExpression { operator: Operator::Divide, lhs, rhs }
     }
     pub fn integer(int: i32) -> Self {
-        ValueExpression(int)
+        Expression::ValueExpression(int)
     }
-    pub fn block<'a>(expressions: &'a [Expression<'a>]) -> Self {
-        BlockExpression { expressions }
+    pub fn block(expressions: &'a [Self]) -> Self {
+        Expression::BlockExpression { expressions }
     }
-    pub fn while_exp<'a>(condition: Expression<'a>, body: Expression<'a>) -> Self {
-        WhileExpression { condition: Box::new(condition), body: Box::new(body) }
+    pub fn while_exp(condition: &'a Self, body: &'a Self) -> Self {
+        Expression::WhileExpression { condition, body }
     }
-    pub fn if_exp<'a>(condition: Expression<'a>, then_clause: Expression<'a>, else_clause: Option<Expression<'a>>) -> Self {
-        IfExpression { condition: Box::new(condition), then_clause: Box::new(body), else_clause }
+    pub fn if_exp(condition: &'a Self, then_clause: &'a Self, else_clause: &'a Option<Self>) -> Self {
+        Expression::IfExpression { condition, then_clause, else_clause }
     }
 }
 
-pub fn add<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
+pub fn add<'a>(lhs: &'a Expression<'a>, rhs: &'a Expression<'a>) -> Expression<'a> {
     Expression::add(lhs, rhs)
 }
 
-pub fn subtract<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
+pub fn subtract<'a>(lhs: &'a Expression<'a>, rhs: &'a Expression<'a>) -> Expression<'a> {
     Expression::subtract(lhs, rhs)
 }
 
-pub fn multiply<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
+pub fn multiply<'a>(lhs: &'a Expression<'a>, rhs: &'a Expression<'a>) -> Expression<'a> {
     Expression::multiply(lhs, rhs)
 }
 
-pub fn divide<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
+pub fn divide<'a>(lhs: &'a Expression<'a>, rhs: &'a Expression<'a>) -> Expression<'a> {
     Expression::divide(lhs, rhs)
 }
 
-pub fn integer(int: i32) -> Expression<'static> {
+pub fn integer<'a>(int: i32) -> Expression<'a> {
     Expression::integer(int)
 }
 
@@ -95,16 +95,12 @@ pub fn block<'a>(expressions: &'a [Expression<'a>]) -> Expression<'a> {
     Expression::block(expressions)
 }
 
-pub fn while_exp<'a>(condition: Expression<'a>, body: Expression<'a>) -> Expression<'a> {
+pub fn while_exp<'a>(condition: &'a Expression<'a>, body: &'a Expression<'a>) -> Expression<'a> {
     Expression::while_exp(condition, body)
 }
 
-pub fn if_exp<'a>(condition: Expression<'a>, then_clause: Expression<'a>) -> Expression<'a> {
-    Expression::if_exp(condition, then_clause, None)
-}
-
-pub fn if_else_exp<'a>(condition: Expression<'a>, then_clause: Expression<'a>, else_clause: Expression<'a>) -> Expression<'a> {
-    Expression::if_exp(condition, then_clause, Some(else_clause))
+pub fn if_else_exp<'a>(condition: &'a Expression<'a>, then_clause: &'a Expression<'a>, else_clause: &'a Option<Expression<'a>>) -> Expression<'a> {
+    Expression::if_exp(condition, then_clause, else_clause)
 }
 
 pub type Value = i32;
